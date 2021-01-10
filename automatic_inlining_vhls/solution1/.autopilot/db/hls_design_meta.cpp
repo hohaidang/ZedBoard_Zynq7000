556bb08e1aab13d28648d90b174f84460e7938d4
hohaidang <hohaidang.khtn@gmail.com>
2021-01-10 16:20:00 +0900

    Restructure

--- automatic_inlining_vhls/solution1/.autopilot/db/hls_design_meta.cpp
+++
@@ -1,7 +0,0 @@
-#include "hls_design_meta.h"
-const Port_Property HLS_Design_Meta::port_props[]={
-	Port_Property("a", 1, hls_in, 0, "ap_none", "in_data", 1),
-	Port_Property("b", 1, hls_in, 1, "ap_none", "in_data", 1),
-	Port_Property("c", 1, hls_out, 2, "ap_none", "out_data", 1),
-};
-const char* HLS_Design_Meta::dut_name = "automatic_inlining";
